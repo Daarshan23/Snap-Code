bf14021bbd5548ddfd9923ae26ee9666eb836df0
daaaanav <dj631125@gmail.com>
2024-01-02 17:49:19 +0530

    Up

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
     <div className="flex items-center gap-10 py-16">
       <Link 
         className="text-sm font-medium hover:text-slate-200 ease-in-out transition-all duration-500"
-        href="https://www.instagram.com/daarshan.23/" target="_blank"
+        href="https://www.instagram.com/daarshan.23/" target="_blank" 
       >
         Follow Me &nbsp;&#129109;
       </Link>
@@ -21,7 +21,7 @@ function Footer() {
         <span className="pl-1 font-medium text-slate-200">DJ-Dev</span>
       </Link>
       <Link
-        href="https://github.com/Maclinz/code-to-img"
+        href="https://github.com/Daarshan23/Snap-Code"
         className="flex items-center text-sm font-medium hover:text-slate-200 ease-in-out transition-all duration-500"
       >
         Source Code Here &nbsp;&#129109;
